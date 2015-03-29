a16e13dc0faed64443a060ec7d5836b126d8f3b0
David R. MacIver <david@drmaciver.com>
2015-03-29 09:37:01 +0200

    Test for calling a subprocess
    This example courtesy of @Aryx in issue #44.
    
    It works fine on my machine but lets see what Appveyor has to say
    about it.

---
+++ tests/test_subprocess.py
@@ -0,0 +1,23 @@
+import os
+import sys
+import subprocess
+
+from hypothesis import given
+
+
+@given(str)
+def test_exact_details_irrelevant(s):
+    """
+    Just here to ensure that there's a @given call
+    """
+    pass
+
+
+def test_subprocess():
+    """
+    Check that Hypothesis's path changes haven't broken the ability to us
+    it in the env of a subprocess
+    """
+    env = os.environ.copy()
+    env["PYTHONPATH"] = os.pathsep.join(sys.path)
+    subprocess.check_call(["python", "--version"], env=env)
