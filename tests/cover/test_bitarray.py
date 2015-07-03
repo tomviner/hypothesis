7b7518d35436b36696ac4289e6aed90d6c1fefb9
David R. MacIver <david@drmaciver.com>
2015-07-03 09:58:09 +0100

    Test out of bounds behaviour for bitarray

--- tests/cover/test_bitarray.py
+++ tests/cover/test_bitarray.py
@@ -1,3 +1,5 @@
+import pytest
+
 from hypothesis.internal.bitarray import BitArray
 from hypothesis import given
 import hypothesis.strategies as st
@@ -13,3 +15,12 @@ def test_boolean_is_equivalent_to_a_bitarray(xs):
 
     for i in hrange(len(xs)):
         assert arr[i] == xs[i]
+
+
+def test_bitarray_errors_on_out_of_bounds_access():
+    x = BitArray(10)
+    for i in (-2, -1, 10, 11):
+        with pytest.raises(IndexError):
+            x[i] = True
+        with pytest.raises(IndexError):
+            x[i]
